21993754bcae2030c9336cd5acc88b88b1f8bd6b
David <davidkiptum80@gmail.com>
2025-07-28 10:19:25 +0300

    adds handlesearch

---
+++ resources/handlesearch.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/spaces", methods=["GET"])
+def get_spaces():
+    
+    activity = request.args.get("activity")
+    location = request.args.get("location")
+
+    
+    filtered = spaces
+
+    if activity:
+        filtered = [s for s in filtered if s["activity"].lower() == activity.lower()]
+    if location:
+        filtered = [s for s in filtered if s["location"].lower() == location.lower()]
+
+    return jsonify(filtered)
+
+if __name__ == "__main__":
+    app.run(debug=True)
